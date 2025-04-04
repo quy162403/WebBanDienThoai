7c8dbd039a6ab0a226fb29fbf230922431269529
quy1603 <nguyenngocquy24032003@gmail.com>
2025-04-04 23:41:04 +0700

    project mon web

---
+++ project_Mon_Wed/FE/Layouts-FE/footer.php
@@ -0,0 +1,47 @@
+<?php
+    $baseURL ='';
+    require_once ($baseUrl.'./utils/ulitity.php');
+    require_once ($baseUrl.'./database/dbhelper.php');
+?>
+<div class="footer">
+    <div class="footer-1">
+        <div class="grid wide footer-items">
+            <div class="row">
+                <div class="col l-3">
+                    <span class="footer-title">NHÓM ĐIỆN THOẠI</span>
+                    <div class="footer-gioithieu">NHÓM ĐIỆN THOẠI là một nhóm thành lập ra trang wed. Nhóm điện thoại sẽ mang đến
+                        cho khách hàng những trải nghiệm tuyệt vời nhất.
+                    </div>
+                </div>
+                
+                <div class="col l-3">
+                    <span class="footer-title">THÔNG TIN HỖ TRỢ</span>
+                    <a href="#" class="tag-a"><div class="footer-inform">Mua hàng và thanh toán online</div></a>
+                    <a href="#" class="tag-a"><div class="footer-inform">Tra thông tin bảo hành</div></a>
+                    <a href="#" class="tag-a"><div class="footer-inform">Quy định về việc sao lưu dữ liệu</div></a>
+                    <a href="#" class="tag-a"><div class="footer-inform">Chính sách bảo mật</div></a>
+                    <a href="#" class="tag-a"><div class="footer-inform-last">Ưu đãi từ đối tác</div></a>
+                </div>
+                <div class="col l-3">
+                    <span class="footer-title">THÔNG TIN VÀ HƯỚNG DẪN</span>
+                    <a href="#" class="tag-a"><div class="footer-inform">Giao hàng và thanh toán</div></a>
+                    <a href="#" class="tag-a"><div class="footer-inform">Chế độ bảo hành</div></a>
+                    <a href="#" class="tag-a"><div class="footer-inform">Nhận hàng và đổi trả</div></a>
+                    <a href="#" class="tag-a"><div class="footer-inform">Đập hộp và nhận quà</div></a>
+                    <a href="#" class="tag-a"><div class="footer-inform-last">Quy chế hoạt động</div></a>
+                </div>
+                <div class="col l-3">
+                    <span class="footer-title">ĐỊA CHỈ CỬA HÀNG</span>
+                    <div class="footer-address">Hà Nội</div>
+                    <div class="footer-address">Số 01, Trường Đại Học Thủy Lợi</div>
+                    <div class="footer-address">Số 02, 178 Tây Sơn</div>
+                    <div class="footer-address">Số 04, 29 Chùa Bộc</div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="footer-end">
+            <span></span>
+        </div>
+    </div>
+</div>
